e41db09d148187987147d4cb50c52b72fdf213bd
Lokesh suwalka <105945491+Lokeshsuwalka05@users.noreply.github.com>
2024-05-03 00:06:40 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,6 +8,29 @@
 */
 
 function isAnagram(str1, str2) {
+  let s=str1.toLowerCase();
+  let t=str2.toLowerCase();
+  
+  const mp=new Map();
+  if(s.length!=t.length){
+      return false;
+  }
+  for(const char of s){
+      mp.set(char,(mp.get(char)||0)+1);
+  }
+  for(const char of t){
+      if(!mp.has(char)){
+          return false;
+      }
+
+      mp.set(char,(mp.get(char))-1);
+  }
+  for(const [key,value] of mp){
+      if(value!=0){
+          return false;
+      }
+  }
+  return true;
 
 }
 
